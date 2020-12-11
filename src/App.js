b93124f38a30e63cbb1bbfedcf1a107bf9d9330d
Polinet <uchpolinet@gmail.com>
2020-12-11 11:19:27 +1100

    stop watch is functioning

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 const App = () => {
   const [timer, setTimer] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const [isStopped, setIsStopped] = useState(false);
   const countRef = useRef(null);
 
   const handleStartTimer = () => {
@@ -12,16 +13,21 @@ const App = () => {
       setTimer((timer) => timer + 1)
     }, 1000)
   };
+
+  const handleStop = () => {
+    setIsStopped(true)
+    clearInterval(countRef.current)
+  };
   
   return (
     <div className="App">
       <h1>Stopwatch with Redux</h1>
-      <h1>00:00:00</h1>
+      <h1>00:00:{timer}</h1>
       <button>+</button>
       <button onClick={handleStartTimer}>Start</button>
+      <button onClick={handleStop}>Stop</button> 
       <button>Lap</button>
       <button>Reset</button>
-      <button>Stop</button> 
       <button>-</button>
     </div>
   );
