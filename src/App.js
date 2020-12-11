d7b95676f72201b28161b8c6f796dbaa4b45ef92
Polinet <uchpolinet@gmail.com>
2020-12-11 12:14:02 +1100

    reset & increment time is completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
   const [timer, setTimer] = useState(0);
   const [isActive, setIsActive] = useState(false);
   const [isStopped, setIsStopped] = useState(false);
+  const [incrementByOne, setIncrementByOne] = useState(0);
+  const [decrementByOne, setDecrementByOne] = useState(0);
   const countRef = useRef(null);
 
   const handleStartTimer = () => {
@@ -15,19 +17,30 @@ const App = () => {
   };
 
   const handleStop = () => {
-    setIsStopped(true)
-    clearInterval(countRef.current)
+    setIsStopped(true);
+    clearInterval(countRef.current);
   };
   
+  const handleReset = () => {
+    clearInterval(countRef.current);
+    setIsStopped(true);
+    setIsActive(true);
+    setTimer(0);
+  }
+
+  const handleIncrement = () => {
+    setIncrementByOne(incrementByOne + 1);
+  }
+
   return (
     <div className="App">
       <h1>Stopwatch with Redux</h1>
       <h1>00:00:{timer}</h1>
-      <button>+</button>
+      <button onClick={handleIncrement}>+</button>
       <button onClick={handleStartTimer}>Start</button>
       <button onClick={handleStop}>Stop</button> 
       <button>Lap</button>
-      <button>Reset</button>
+      <button onClick={handleReset}>Reset</button>
       <button>-</button>
     </div>
   );
